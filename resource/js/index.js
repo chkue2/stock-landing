d34ef7fa4f0360b02df2f1b8208a0bf1deb344c2
chkue2 <chkue0924@gmail.com>
2022-07-23 22:55:28 +0900

    update

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,41 +1,41 @@
 const reviews = [
   {
-    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br><br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
+    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
     date: '2022.07.01',
     name: '박귀리'
   },
   {
-    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br><br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
+    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
     date: '2022.07.01',
     name: '박귀리'
   },
   {
-    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br><br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
+    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
     date: '2022.07.01',
     name: '박귀리'
   },
   {
-    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br><br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
+    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
     date: '2022.07.01',
     name: '박귀리'
   },
   {
-    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br><br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
+    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
     date: '2022.07.01',
     name: '박귀리'
   },
   {
-    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br><br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
+    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
     date: '2022.07.01',
     name: '박귀리'
   },
   {
-    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br><br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
+    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
     date: '2022.07.01',
     name: '박귀리'
   },
   {
-    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br><br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
+    content: '오 저 수익률 이렇게 좋은거 처음봐여 역시,,, 초보혼자할 때랑 차원이 다르네요<br>요즘 계속 하락장이어서 수익 본지 언젠지도 모르겠는데ㅠㅠ 덕분에 돈 벌었어요!!',
     date: '2022.07.01',
     name: '박귀리'
   }
@@ -45,9 +45,14 @@ $(document).ready(function(){
   setReviews()
   const swiper1 = new Swiper("#reviews", {
     slidesPerView: 'auto',
-    spaceBetween: 40,
+    spaceBetween: 12,
     observeParents: true,
     observer: true,
+    breakpoints: {
+      1299: {
+        spaceBetween: 40,
+      }
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -83,7 +88,56 @@ const setReviews = () => {
 
 const setActiveClass = (index) => {
   $('#reviews .swiper-slide').removeClass('active')
-  for(i = index; i <= index + 2; i++){
-    $('#reviews .swiper-slide').eq(i).addClass('active')
+  
+  if(window.innerHeight >= 1300){
+    for(i = index; i <= index + 2; i++){
+      $('#reviews .swiper-slide').eq(i).addClass('active')
+    }
+  } else {
+    $('#reviews .swiper-slide').eq(index).addClass('active')
   }
+}
+
+$(document).on('click', '.button', function(){
+  openFormModal()
+})
+
+const openFormModal = () => {
+  $('#formModal').show()
+}
+
+const closeFormModal = () => {
+  $('#formModal').hide()
+}
+
+const sendForm = () => {
+  const name = $('#name').val()
+  const tel1 = $('#tel1').val()
+  const tel2 = $('#tel2').val()
+  const tel3 = $('#tel3').val()
+  const tel = `${tel1}-${tel2}-${tel3}`
+  const userAgree = $('#userAgree').is(':checked')
+  const serviceAgree = $('#serviceAgree').is(':checked')
+
+  if(name === ''){
+    alert('이름을 입력해주세요')
+    return false
+  } else if(tel1.length < 3 || tel2.length < 3 || tel3.length < 4 ){
+    alert('핸드폰번호를 입력해주세요')
+    return false
+  } else if(userAgree === false){
+    alert('개인정보 처리 방침에 동의해주세요')
+    return false
+  } else if(serviceAgree === false){
+    alert('서비스 이용 약관에 동의해주세요')
+    return false
+  } 
+
+
+  /*
+    api request parameter
+
+    name: name,
+    tel: tel,
+  */
 }
